e4bdf245539041503bcb63fe278381aab1132052
thanaraklee <bzank.lee@hotmail.com>
2025-05-17 22:48:05 +0700

    UP: Update ini-deploy

--- ini-deploy.sh
+++ ini-deploy.sh
@@ -6,7 +6,7 @@ sudo apt-get update -y
 
 # Install Python
 ## Compiling package
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget curl
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget curl -y
 ## Download Python into /tmp directory
 cd /tmp
 curl -O --verbose https://www.python.org/ftp/python/3.13.3/Python-3.13.3.tgz
@@ -14,10 +14,42 @@ tar -xf Python-3.13.3.tgz
 ## Evaluates and prepares Python
 cd Python-3.13.3
 ./configure --enable-optimizations
+sudo make install
 cd $HOME
 
+sudo apt-get update -y
+if [ ! -d ".venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv .venv
+else
+    echo "Virtual environment already exists."
+fi
+
+# Activate the virtual environment
+# Check the operating system and activate the virtual environment accordingly
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # For macOS
+  source .venv/bin/activate
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # For Linux
+  source .venv/bin/activate
+elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+  # For Windows (Git Bash or Cygwin)
+  source .venv/Scripts/activate
+else
+  echo "Unsupported operating system: $OSTYPE"
+  exit 1
+fi
+
+# install the project in editable mode
+pip3 install -e .
+
+# Upgrade pip and install required packages
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
 # Add Docker's official GPG key:
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -30,7 +62,8 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 sudo docker run hello-world
+
 
